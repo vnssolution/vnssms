18bbbe9f965a4222387a4d42641aab96f5f3e26c
jalendrav <jalendra@jobsmama.com>
2020-09-09 10:38:05 +0530

    Backup File removed

--- srcbkp/app/app.api-endpoints.ts
+++
@@ -1,9 +0,0 @@
-export class AppApiEndpoints{
-    public UserSignUp= "signUp";
-    public Userlogin= "login";
-    public uploadContacts = "uploadContacts";
-    public contactList = "contactList";
-    public verifyPhone = "verifyPhone";
-    public createGroup = "createGroup";
-    public groupList = "groupList";
-}
